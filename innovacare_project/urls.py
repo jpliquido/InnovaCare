51d576b999cab236f4d5ed2ff51b9c25aca88d8a
joms <joms.liquido@gmail.com>
2024-06-30 21:44:42 +0800

    Added Pages App

--- innovacare_project/urls.py
+++ innovacare_project/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from re import template
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('pages.urls')),
 ]
